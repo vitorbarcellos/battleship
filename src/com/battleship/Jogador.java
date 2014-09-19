27eb00e49a86b1defc7f27b10e65681f60f7038e
Vitor Barcellos <vitor.barcellos@terra.com.br>
2014-09-19 20:45:08 -0300

    Adicionado a Classe Jogador

---
+++ src/com/battleship/Jogador.java
@@ -0,0 +1,29 @@
+package com.battleship;
+
+public class Jogador {
+		
+		
+		private int pontos = 0;
+
+	public Jogador() {
+		
+	}
+
+	public void exibe() {
+		
+	}
+	
+	public void exibeOculto() {
+		
+	}
+
+	public int getPontos() {
+		return pontos;
+	}
+
+	public void ganha() {
+		this.pontos++;
+		
+	}
+
+}
